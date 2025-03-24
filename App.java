8c0a08a2ac5899f97184c9e51e6e2b9d860b0a3d
Art Guerrero <artemio.guerreru@g.austincc.edu>
2025-03-23 21:39:48 -0500

    create a seperate function for checking if number is a prime number

--- App.java
+++ App.java
@@ -12,16 +12,43 @@ public class App {
         
         // while the input is invalid do this
         while (!valindInput) {
-            System.out.println("Input a integer and I will let you know if it is a prime number: ");
+            System.out.println("Input any positive integer and I will let you know if it is a prime number: ");
             // if input is an integer
             if (sc.hasNextInt()) {
                 // number is the user input if it is an integer
                 number = sc.nextInt();
-                // flip boolean to true to move on from while loop if user inputs a integer
-                valindInput = true;
+
+                if (number > 0) {
+                    // flip boolean to true to move on from while loop if user inputs a positive integer
+                    valindInput = true;
+                }
+                else{
+                    System.out.println("Invalid Input! Please enter a positive integer.");
+                } 
+            }
+            // if input is invalid do this:
+            else{
+                // Alert user to invalid entry
+                System.out.println("Invalid Input!");
+                // clear input to run again
+                sc.next();
             }
 
+
         }
+    }
+    public static boolean isPrime(int n){
+        if (n <= 1 ) {
+            return false;
+        }
+        // Check divisibility from 2 to sqrt(n)
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false; // If divisible, it's not a prime number
+            }
+        }
+
+        return true; // If no divisors found, the number is prime
     }
 }
 
