e32a34d15b3e74b30054814d6992abf5149c7460
Art Guerrero <artemio.guerreru@g.austincc.edu>
2025-03-23 21:14:39 -0500

    while the input is invalid do this,if input is an integer,number is the user input if it is an integer,flip boolean to true to move on from while loop if user inputs a integer

--- App.java
+++ App.java
@@ -10,5 +10,20 @@ public class App {
         boolean valindInput =  false;
         // cteate bool variable for input validation
         
+        // while the input is invalid do this
+        while (!valindInput) {
+            System.out.println("Input a integer and I will let you know if it is a prime number: ");
+            // if input is an integer
+            if (sc.hasNextInt()) {
+                // number is the user input if it is an integer
+                number = sc.nextInt();
+                // flip boolean to true to move on from while loop if user inputs a integer
+                valindInput = true;
+            }
+
+        }
     }
 }
+
+
+
