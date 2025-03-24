0de7948a87722b8989e9d4169d7850bb5c2483f9
Art Guerrero <artemio.guerreru@g.austincc.edu>
2025-03-23 21:03:28 -0500

    import scanner for user input,create a number variable,create variable to capture user input,cteate bool variable for input validation

--- App.java
+++ App.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+// import scanner for user input
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int number = 0;
+        // create a number variable
+        Scanner sc = new Scanner(System.in);
+        // create variable to capture user input
+        boolean valindInput =  false;
+        // cteate bool variable for input validation
+        
     }
 }
